02d47d0a90d45fcab558f82d7dacfeef75cb5ae6
leonardolasso <lassoleonardo56@gmail.com>
2024-06-11 20:49:50 -0500

    inicial project star .bat

---
+++ index.js
@@ -0,0 +1,13 @@
+//importaciones
+
+
+//conexion a la base de datos
+
+
+//configuracion de los  cors las peticiones de los diferentes dominios
+
+//Convercion de datos (body a objetos JS)
+
+//configuracion de las rutas
+
+//Configuracion el servidor para escuchar las peticiones http
